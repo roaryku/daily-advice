c6593506e56804b3a0594e7909930bd560b7e4d9
Rodika Aryku <raryku@yahoo.com>
2024-04-09 16:46:09 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ 
+     const [advice, setAdvice] = useState("");
 
-export default App;
+    useEffect(() => {
+      getAdvice();
+    }, []);
+
+     const getAdvice = async () => {
+      const responce = await fetch("https://api.adviceslip.com/advice ")
+      const data = await responce.json();
+      setAdvice(data.slip.advice)
+     }
+      getAdvice();
+
+
+   return(
+    <div className="App">
+      <h1>Find your daily advice here!!!</h1>
+      <p>{advice}</p>  
+      <button onClick={getAdvice}>Get New Advice</button>
+    </div>
+   )
+}
+  export default App;
+
+
+
